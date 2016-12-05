1b927c645b2c6bc3f2c2b57660b292cfb1649410
advancevillage <cugriver@gmail.com>
2016-12-05 21:00:09 +0800

    ubuntu-vim-install.sh

---
+++ ubuntu-vim-install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#root permission needed
+
+#install vim
+if test -e /usr/bin/vim || test -e /usr/local/bin/vim || test -e /bin/vim
+then
+	echo 'vim has existed'
+else
+	echo 'vim not existed and will install vim!'
+	apt-get install -y vim
+fi
+
+#install git
+if test -e  /usr/bin/git || test -e /usr/local/bin/git || test -e /bin/git
+then
+	echo 'git has existed'
+else 
+	echo 'git not existed and will install git'
+	apt-get install -y git
+fi
+
+#install gcc
+if test -e /usr/bin/gcc || test -e /usr/local/bin/gcc || test -e /bin/gcc
+then
+	echo 'gcc has existed'
+else
+	echo 'git not existed and will install gcc*'
+	apt-get install -y gcc*
+fi
+
+#install cmake
+if test -e /usr/bin/cmake || test -e /usr/local/bin/cmake || test -e /bin/cmake
+then 
+	echo 'cmake has existed'
+else
+	echo 'cmake not existed and will install cmake'
+	apt-get install -y cmake
+fi
+
+apt-get install -y python-dev
+
+GitPath=`/usr/bin/which git`
+if test -d ~/.vim
+then
+    FileCount=`ls ~/.vim/ | wc -l`
+    if [ ${FileCount} -eq 0 ]
+    then
+        git clone https://github.com/advancevillage/vim.git ~/.vim
+    fi
+else 
+    git clone https://github.com/advancevillage/vim.git ~/.vim
+fi
+
